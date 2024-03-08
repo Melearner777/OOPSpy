b87fdb39f20f2fc877b85359e5bfa6e4eac962ee
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-03-08 19:45:49 +0530

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,11 @@
+class Employee:
+    Employee_id=0
+
+Employee1=Employee()
+Employee2=Employee()
+
+Employee1.Employee_id=1010
+print(f"Employee iD:{Employee1.Employee_id}")
+
+Employee2.Employee_id=1001
+print(f"Employee id:{Employee2.Employee_id}")
