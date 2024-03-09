c5980db152cfb1162c213c3272340a2a1ef9dfa9
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-03-09 23:06:50 +0530

    Add files via upload

--- constructor.py
+++ constructor.py
@@ -2,17 +2,18 @@ class Person:
     
 
     def __init__(self,name,o):
-        
+        print("Hey,I am a person") #This will everytime print if object will created.
         self.name=name
         self.occ=o
 
     def info(self):
-        print("Hey,I am a person")
+        
         print(f"{self.name} is a {self.occ}")
 
 a = Person("Jishan","Student")
 b = Person("Divya","ALP")
 a.info()
+b.info()
 b.info()
 
 
