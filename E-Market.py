f9f600d862b4b2fa8ca04ed37748a19c810a006f
MD Jishan <152049778+Melearner777@users.noreply.github.com>
2024-03-25 19:22:35 +0530

    Add files via upload

---
+++ E-Market.py
@@ -0,0 +1,26 @@
+class Emarket:
+    def __init__(self,name):
+        self.name=name
+        self.items=[]
+    def __getitem__(self,i):
+        return self.items[i]
+    def __len__(self):
+        return len(self.items)
+    
+    def __str__(self):
+        return f"Emarket have {len(self)} items"
+    def __repr__(self):
+        return f"{self.name}:['Electronics','Groceries','Plates','Glass']"
+
+E1=Emarket("NO NAME")
+
+E1.items.append('Electronics')
+E1.items.append('Plates')
+E1.items.append('Groceries')
+E1.items.append('Glass')
+
+print(E1.items)
+for i in E1:
+    print(i)
+
+print(E1)
